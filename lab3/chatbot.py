1b4243fefd3d498b1e31fbc632a5e2e44789dc20
rosta233 <23455993@life.hkbu.edu.hk>
2024-02-17 21:49:25 +0800

    new file

--- lab3/chatbot.py
+++ lab3/chatbot.py
@@ -1,1 +1,37 @@
+import telegram
+from telegram.ext import Updater, MessageHandler, Filters
+import configparser
+import logging
 
+
+def main():
+    # Load your token and create an Updater for your Bot
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    updater = Updater(token=config['TELEGRAM']['ACCESS_TOKEN'], use_context=True)
+    dispatcher = updater.dispatcher
+
+    # Set up logging
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+    # Register a dispatcher to handle message
+    echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+    dispatcher.add_handler(echo_handler)
+
+    # Start the bot
+    updater.start_polling()
+    updater.idle()
+
+
+def echo(update, context):
+    reply_message = update.message.text.upper()
+    logging.info("Update: " + str(update))
+    logging.info("Context: " + str(context))
+    try:
+        context.bot.send_message(chat_id=update.effective_chat.id, text=reply_message)
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
+
+
+if __name__ == '__main__':
+    main()
